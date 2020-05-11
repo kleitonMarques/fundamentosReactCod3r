4896e2d875f752a39de6b9e181a2f40865566d5d
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-11 19:31:16 -0300

    Adicionando os exercicios em Cards

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import React from 'react'
+
+import Primeiro from './components/Primeiro'
+import ComParamentro from './components/ComParamentro'
+import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
+
+export default props => (
+    <div className="App">
+        <Card titulo="#03 - Componente com Filhos">
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
+        </Card>   
+
+        <Card titulo="#02 - Componente com Parametro">
+            <ComParamentro
+                titulo="Esse é o título"
+                subtitulo="Esse é o subtítulo">
+            </ComParamentro>
+        </Card>     
+
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro></Primeiro>
+        </Card>
+    </div>
+)
--- src/index.js
+++ src/index.js
@@ -2,23 +2,9 @@ import './index.css'
 import React from 'react'
 import ReactDom from 'react-dom'
 
-import ComFilhos from './components/ComFilhos'
+import App from './App'
 
 ReactDom.render(
-    <div>
-        <ComFilhos>
-            <ul>
-                <li>Ana</li>
-                <li>Bia</li>
-                <li>Carlos</li>
-                <li>Daniel</li>
-            </ul>
-        </ComFilhos>
-        {/* <Primeiro></Primeiro>
-        <ComParamentro
-            titulo="Esse é o título"
-            subtitulo="Esse é o subtítulo">
-        </ComParamentro> */}
-    </div>,
+    <App></App>,
     document.getElementById('root')
 )

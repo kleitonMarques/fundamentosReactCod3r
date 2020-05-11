29e2f30f7f33ddddc766fa0063b970973fc05d27
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-11 19:02:22 -0300

    PROPS.CHILDREN

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import './index.css'
 import React from 'react'
 import ReactDom from 'react-dom'
 
-import Primeiro from './components/Primeiro'
-import ComParamentro from './components/ComParamentro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDom.render(
     <div>
-        <Primeiro></Primeiro>
+        <ComFilhos>
+            <ul>
+                <li>Ana</li>
+                <li>Bia</li>
+                <li>Carlos</li>
+                <li>Daniel</li>
+            </ul>
+        </ComFilhos>
+        {/* <Primeiro></Primeiro>
         <ComParamentro
             titulo="Esse é o título"
             subtitulo="Esse é o subtítulo">
-        </ComParamentro>
+        </ComParamentro> */}
     </div>,
     document.getElementById('root')
 )

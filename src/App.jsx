a57939fcd3c887e8681a7493f94b53b058fc5115
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-18 16:11:53 -0300

    Contador

--- src/App.jsx
+++ src/App.jsx
@@ -11,53 +11,62 @@ import CondicionalComIf from './components/basicos/CondicionalComIf'
 import Pai from './components/comunicacao/direta/Pai'
 import Super from './components/comunicacao/indireta/Super'
 import Input from './components/form/Input'
+import Contador from './components/contador/Contador'
 
 export default props => (
     <div className="App">
-        <Card titulo="#09 - Input" color="#9C0F5F">
-            <Input></Input>
-        </Card>
+        <h1>Fundamentos React</h1>
 
-        <Card titulo="#08 - Comunicação Indireta" color="#000">
-            <Super></Super>
-        </Card>
+        <div className="Cards">
+            <Card titulo="#10 - Contador" color="#293E6A">
+                <Contador passo={10} valor={100}></Contador>
+            </Card>
 
-        <Card titulo="#07 - Comunicação Direta" color="#4298B5">
-            <Pai sobrenome="Freitas"></Pai>
-        </Card>
+            <Card titulo="#09 - Input" color="#9C0F5F">
+                <Input></Input>
+            </Card>
 
-        <Card titulo="#06 - Condicional v2" color="#FA6900">
-            <CondicionalComIf numero={11}></CondicionalComIf>
-        </Card> 
+            <Card titulo="#08 - Comunicação Indireta" color="#000">
+                <Super></Super>
+            </Card>
 
-        <Card titulo="#05 - Condicional v1" color="#E94C6F">
-            <Condicional numero={10}></Condicional>
-        </Card> 
+            <Card titulo="#07 - Comunicação Direta" color="#4298B5">
+                <Pai sobrenome="Freitas"></Pai>
+            </Card>
 
-        <Card titulo="#04 - Repetição" color="#008BBA">
-            <Repeticao></Repeticao>
-        </Card> 
+            <Card titulo="#06 - Condicional v2" color="#FA6900">
+                <CondicionalComIf numero={11}></CondicionalComIf>
+            </Card> 
 
-        <Card titulo="#03 - Componente com Filhos" color="#D96459">
-            <ComFilhos>
-                <ul>
-                    <li>Ana</li>
-                    <li>Bia</li>
-                    <li>Carlos</li>
-                    <li>Daniel</li>
-                </ul>
-            </ComFilhos>
-        </Card>   
+            <Card titulo="#05 - Condicional v1" color="#E94C6F">
+                <Condicional numero={10}></Condicional>
+            </Card> 
 
-        <Card titulo="#02 - Componente com Parametro" color="#FF85CB">
-            <ComParamentro
-                titulo="Esse é o título"
-                subtitulo="Esse é o subtítulo">
-            </ComParamentro>
-        </Card>     
+            <Card titulo="#04 - Repetição" color="#008BBA">
+                <Repeticao></Repeticao>
+            </Card> 
 
-        <Card titulo="#01 - Primeiro Componente" color="#92B06A">
-            <Primeiro></Primeiro>
-        </Card>
+            <Card titulo="#03 - Componente com Filhos" color="#D96459">
+                <ComFilhos>
+                    <ul>
+                        <li>Ana</li>
+                        <li>Bia</li>
+                        <li>Carlos</li>
+                        <li>Daniel</li>
+                    </ul>
+                </ComFilhos>
+            </Card>   
+
+            <Card titulo="#02 - Componente com Parametro" color="#FF85CB">
+                <ComParamentro
+                    titulo="Esse é o título"
+                    subtitulo="Esse é o subtítulo">
+                </ComParamentro>
+            </Card>     
+
+            <Card titulo="#01 - Primeiro Componente" color="#92B06A">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
 )

b6d978246966b1aec5f54d6d65933911a517f0c8
Kleiton Marques <kleitonm.marques@gmail.com>
2020-05-18 14:23:46 -0300

    Comunicação Indireta entre componentes

--- src/components/comunicacao/indireta/Super.jsx
+++ src/components/comunicacao/indireta/Super.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sub from './Sub'
 
 export default props => {
+    const [texto, setTexto] = useState('Valor')
+    const [num, setNum] = useState(0)
 
     function quandoClicar(valorGerado, texto) {
-        console.log('Ação!!!!')
-        console.log(valorGerado)
-        console.log(texto)
+        setNum(valorGerado)
+        setTexto(texto)
     }
 
     return (
         <div>
-            <h4>Valor</h4>
+            <h4>{texto}: {num}</h4>
             <Sub onClicar={quandoClicar}></Sub>
         </div>
     )
